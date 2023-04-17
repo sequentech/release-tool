ba1be4b6ec8ddeceb001ab8f785e7f2067de6ea7
Eduardo Robles Elvira <edu@sequentech.io>
2023-04-17 15:25:30 +0200

    adding copyright headers

--- tests_update_parent_issue.py
+++ tests_update_parent_issue.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023 Sequent Tech Inc <legal@sequentech.io>
+#
+# SPDX-License-Identifier: AGPL-3.0-only
 import unittest
 from unittest.mock import MagicMock, patch
 from github import Github, Project, Issue
