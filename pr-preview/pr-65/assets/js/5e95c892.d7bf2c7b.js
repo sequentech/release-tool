3c1489dbdc4427cd9ef1895007691bb0f9510efa
edulix <edulix@users.noreply.github.com>
2025-12-30 10:40:14 +0000

    Remove preview for PR 65 🛬

--- pr-preview/pr-65/assets/js/5e95c892.d7bf2c7b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkrelease_tool_website=globalThis.webpackChunkrelease_tool_website||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>i});r(6540);var a=r(4164),l=r(7559),t=r(5500),o=r(2831),u=r(1656),c=r(4848);function i(e){return(0,c.jsx)(t.e3,{className:(0,a.A)(l.G.wrapper.docsPages),children:(0,c.jsx)(u.A,{children:(0,o.v)(e.route.routes)})})}}}]);
--- pr-preview/pr-65/assets/js/aba21aa0.ff7145d5.js
+++
@@ -1,1 +0,0 @@
-"use strict";(globalThis.webpackChunkrelease_tool_website=globalThis.webpackChunkrelease_tool_website||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
